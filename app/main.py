2515bdcda94014756d7720ba54a7d193fb32bf64
Yasmim <cavalcantiyasmim213@gmail.com>
2026-08-14 14:37:43 -0300

    remove código comentado e duplicado na configuração do Jinja2 e na rota inicial

--- app/main.py
+++ app/main.py
@@ -24,7 +24,6 @@ app.include_router(usuario_controller.router)
 app.include_router(categoria_controller.router)
 app.include_router(produto_controller.router)
 
-<<<<<<< HEAD
 templates = Jinja2Templates(
     directory="app/templates"
 )
@@ -46,21 +45,6 @@ app.include_router(movimentacoes.router)
 app.include_router(relatorio.router)
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-=======
-@app.get("/")
-def tela_inicial(
-    request: Request,
-    usuario = Depends(get_usuario_opcional)
-):
-    #Tela não logado
-    if usuario is None:
-        return templates.TemplateResponse(
-            request,
-            "index.html",
-            {"request": request}
-        )
-    #Logado - exibir a tela de funcionario
->>>>>>> e04597d8e9c259cf3c1f43c9877f6cffc6bc4ca8
     return templates.TemplateResponse(
         request,
         "home.html",
